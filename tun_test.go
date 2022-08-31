6993e57bff27969dde412aa665cbadf1ad69287f
GuoHaoLan <coopticks@hotmail.com>
2022-08-31 17:08:05 +0800

    Add Tun testing

---
+++ tun_test.go
@@ -0,0 +1,23 @@
+package tun
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTun(t *testing.T) {
+	t := New("tun0", "10.0.0.1", "10.0.0.2")
+	defer t.Close()
+
+	go func() {
+		b := make([]byte, 1500)
+		for {
+			n, err := t.Read(b)
+			if err != nil {
+				return
+			}
+			t.Log(len(n))
+		}
+	}()
+	<-time.After(5 * time.Minute)
+}
